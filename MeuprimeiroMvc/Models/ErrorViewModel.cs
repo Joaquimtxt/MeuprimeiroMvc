bc012ce55db72e15c99c22e26f9c02cb8c1a40ab
Joaquimtxt <jocaq124@gmail.com>
2024-08-27 09:20:29 -0300

    Adicionar arquivos de projeto.

---
+++ MeuprimeiroMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeuprimeiroMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
